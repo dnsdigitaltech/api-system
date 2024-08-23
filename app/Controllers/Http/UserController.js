edcb4b6867ccb445a63d00a127c351a4c964a633
Davi Bernardo <dnssites@gmail.com>
2024-08-23 17:19:25 -0300

    Crud Users

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,24 +1,47 @@
 'use strict'
 
+const User = use('App/Models/User')
 class UserController {
     async index() {
-        return { result: 'Todos users'}
+        return await User.all()
     }
     
-    async show(id) {
-        return { result: 'Usuário epecifico'}
+    async show({ params }) {
+        const user = await User.findOrFail(params.id)
+        return user
     }
 
-    async store() {
-        return { result: 'Salvar'}
+    async store({ request }) {
+        const data = request.only([
+            "name",
+            "username",
+            "email",
+            "password",
+            "type_user_id",
+        ])
+
+        const user = await User.create(data)
+        return user
     }
 
-    async update(id) {
-        return { result: 'Atualizar'}
+    async update({ params, request }) {
+        const user = await User.findOrFail(params.id)
+        const data = request.only([
+            "name",
+            "username",
+            "email",
+            "password",
+            "type_user_id",
+        ])
+
+        user.merge(data)
+        await user.save()
+        return user
     }
     
-    async destroy(id) {
-        return { result: 'remover'}
+    async destroy({ params }) {
+        const user = await User.findOrFail(params.id)
+        return await user.delete()
     }
 }
 
