2e9cde19560719aef647f8ab741f0a355b04a0ea
Davi Bernardo <dnssites@gmail.com>
2024-08-23 19:04:59 -0300

    Crud Produto

--- start/routes.js
+++ start/routes.js
@@ -13,7 +13,15 @@ Route.resource('users', 'UserController').apiOnly().validator(new Map([
 Route.resource('clients', 'ClientController').apiOnly().validator(new Map([
   [['clients.store'], ['Client']],[['clients.update'], ['Client']]
 ]))
+
 Route.resource('exercises', 'ExerciseController').apiOnly().validator(new Map([
   [['exercises.store'], ['Exercise']],[['exercises.update'], ['Exercise']]
 ]))
-Route.resource('trainings', 'TrainingController').apiOnly()
+
+Route.resource('trainings', 'TrainingController').apiOnly().validator(new Map([
+  [['trainings.store'], ['Training']],[['trainings.update'], ['Training']]
+]))
+
+Route.resource('products', 'ProductController').apiOnly().validator(new Map([
+  [['products.store'], ['Product']],[['products.update'], ['Product']]
+]))
