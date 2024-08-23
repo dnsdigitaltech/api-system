dc934df611f6f4be12f1dce28e24c89395af54df
Davi Bernardo <dnssites@gmail.com>
2024-08-23 16:09:23 -0300

    Criando validators para as propriedades

--- start/routes.js
+++ start/routes.js
@@ -6,7 +6,10 @@ Route.get('/', () => {
   return { status: 'Online' }
 })
 
-Route.resource('users', 'UserController').apiOnly()
+Route.resource('users', 'UserController').apiOnly().validator(new Map([
+  [['users.store'], ['User']],[['users.update'], ['User']]
+]))
+
 Route.resource('clients', 'ClientController').apiOnly()
 Route.resource('exercises', 'ExerciseController').apiOnly()
 Route.resource('trainings', 'TrainingController').apiOnly()
