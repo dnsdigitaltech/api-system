8721629fd8bbcd2e2635daed860d293ab9dcc66a
Davi Bernardo <dnssites@gmail.com>
2024-08-23 20:32:08 -0300

    Protegendo as rotas

--- start/routes.js
+++ start/routes.js
@@ -11,20 +11,20 @@ Route.put('/sessions', 'SessionController.refreshToken')
 
 Route.resource('users', 'UserController').apiOnly().validator(new Map([
   [['users.store'], ['User']],[['users.update'], ['User']]
-]))
+])).middleware('auth:jwt')
 
 Route.resource('clients', 'ClientController').apiOnly().validator(new Map([
   [['clients.store'], ['Client']],[['clients.update'], ['Client']]
-]))
+])).middleware('auth:jwt')
 
 Route.resource('exercises', 'ExerciseController').apiOnly().validator(new Map([
   [['exercises.store'], ['Exercise']],[['exercises.update'], ['Exercise']]
-]))
+])).middleware('auth:jwt')
 
 Route.resource('trainings', 'TrainingController').apiOnly().validator(new Map([
   [['trainings.store'], ['Training']],[['trainings.update'], ['Training']]
-]))
+])).middleware('auth:jwt')
 
 Route.resource('products', 'ProductController').apiOnly().validator(new Map([
   [['products.store'], ['Product']],[['products.update'], ['Product']]
-]))
+])).middleware('auth:jwt')
